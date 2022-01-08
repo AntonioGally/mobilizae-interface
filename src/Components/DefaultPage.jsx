e626cf5100cec776a6ffb466dbf5e2123646a21e
AntonioGally <antonio.gally@gmail.com>
2022-01-07 23:41:57 -0300

    feat: when path is '/' redirect to the first stored page

--- src/Components/DefaultPage.jsx
+++ src/Components/DefaultPage.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { mockedData } from "../utils"
+
 
 const DefaultPage = () => {
+  const history = useHistory();
+  useEffect(() => {
+    history.push(`/${mockedData[0].pathName}`)
+  })
   return (
     <h1>This is a default page</h1>
   )
--- src/utils.js
+++ src/utils.js
@@ -8,4 +8,13 @@ export const mockedData = [
     modalImage: "https://cdn.picpng.com/cats/pic-cats-22936.png",
     pathName: 'jardimDasOliveiras'
   },
+  {
+    image: 'https://www.trisul-sa.com.br/blog/wp-content/uploads/2017/03/bairro-arborizado-200317.jpg',
+    district: 'Jardim da penha',
+    containerText: 'Pode ser outro texto pro jardim da penha, mas tem um padrão',
+    buttonText: 'Texto do botão',
+    footerImage: 'https://cdn.picpng.com/cats/pic-cats-22936.png',
+    modalImage: "https://cdn.picpng.com/cats/pic-cats-22936.png",
+    pathName: 'jardimDaPenha'
+  },
 ]
