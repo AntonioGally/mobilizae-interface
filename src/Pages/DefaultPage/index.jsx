fa322ff82c5283bca65bc6e886c54d8646768287
AntonioGally <antonio.gally@gmail.com>
2022-01-18 23:37:54 -0300

    fix: starting to adjust DefaulPage

--- src/Pages/DefaultPage/index.jsx
+++ src/Pages/DefaultPage/index.jsx
@@ -1,20 +1,43 @@
-import React, { useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import { getPages } from "../../scripts/utils"
-import { Spinner } from "react-bootstrap"
+//Libs
+import React from "react";
+
+//styles
+import "./DefaultPage.style.css";
+
+//Assets
+import logoImg from "../../assets/images/defaultPageLogo.png";
+
+//Components
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap"
 
 
 const DefaultPage = () => {
-  const history = useHistory();
-  useEffect(() => {
-    getPages().then((data) => {
-      history.push(`/${data[0].pathName}`)
-    })
-  })
   return (
-    <div style={{ margin: '150px auto', width: 'fit-content' }}>
-      <Spinner animation="border" size="lg" />
-    </div>
+    <>
+      <Navbar collapseOnSelect expand="lg" className="default-page-navbar">
+        <Container>
+          <Navbar.Brand>
+            <img src={logoImg} alt="Mobilizae logo" className="default-page-navbar-image" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link>
+                Listar usuários
+              </Nav.Link>
+              <NavDropdown title="Páginas" id="navbarScrollingDropdown">
+                <NavDropdown.Item >
+                  Listar páginas
+                </NavDropdown.Item>
+                <NavDropdown.Item >
+                  Criar páginas
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   )
 }
 
